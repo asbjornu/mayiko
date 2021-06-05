ab44997d4ec68b9e16a92fbf5b0d3bbc1bc52f82
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2021-06-05 02:21:39 +0200

    Mayiko branding and cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,28 @@
 import { useDeno } from 'framework/react'
 import React from 'react'
 import Logo from '~/components/logo.tsx'
-import useCounter from '~/lib/useCounter.ts'
 
 export default function Home() {
-  const [count, isSyncing, increase, decrease] = useCounter()
   const version = useDeno(() => Deno.version.deno)
 
   return (
     <div className="page">
       <head>
-        <title>Hello World - Aleph.js</title>
+        <title>Welcome to Mayiko</title>
         <link rel="stylesheet" href="../style/index.css" />
+        <link rel="icon" href="/icon.svg" />
+        <link rel="shortcut icon" href="/icon.png" />
       </head>
       <p className="logo"><Logo /></p>
-      <h1>Welcome to use <strong>Aleph.js</strong>!</h1>
-      <p className="links">
-        <a href="https://alephjs.org" target="_blank">Website</a>
-        <span></span>
-        <a href="https://alephjs.org/docs/get-started" target="_blank">Get Started</a>
-        <span></span>
-        <a href="https://alephjs.org/docs" target="_blank">Docs</a>
-        <span></span>
-        <a href="https://github.com/alephjs/aleph.js" target="_blank">Github</a>
-      </p>
-      <div className="counter">
-        <span>Counter:</span>
-        {isSyncing && (
-          <em>...</em>
-        )}
-        {!isSyncing && (
-          <strong>{count}</strong>
-        )}
-        <button onClick={decrease}>-</button>
-        <button onClick={increase}>+</button>
-      </div>
-      <p className="copyinfo">Built by Aleph.js in Deno {version}</p>
+      <h1>Welcome to <strong title="'Mayiko' is chichewan for 'countries'">Mayiko</strong>!</h1>
+      <footer>
+        <p className="copyinfo">
+          Built by Aleph.js in Deno {version}
+        </p>
+        <p className="copyinfo">
+          <a href="https://thenounproject.com/term/countries/3437692/">countries</a> icon by <a href="https://thenounproject.com/kavya261990/">Icongeek26</a> from <a href="https://thenounproject.com/">the Noun Project</a>
+        </p>
+      </footer>
     </div>
   )
 }
