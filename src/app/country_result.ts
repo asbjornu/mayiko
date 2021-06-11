37e21ec8f8b70b6078d8aa379fee4e225386a056
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2021-06-11 22:38:58 +0200

    Map to Country

--- src/app/country_result.ts
+++ src/app/country_result.ts
@@ -1,9 +1,9 @@
-import type { Country } from "./country.ts";
+import { Country } from "./country.ts";
 import type { QueryState } from "./query_state.ts";
 
 export class CountryResult {
   public constructor(countries: Country[], query: QueryState) {
-    this.countries = countries;
+    this.countries = countries.map(x => new Country(x));
     this.query = query;
   }
 
