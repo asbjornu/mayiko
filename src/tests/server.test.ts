d7b95d91d9cf4b3eeccef31d1aca42b93391aeb3
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2021-06-14 23:15:34 +0200

    Make tests less verbose

--- src/tests/server.test.ts
+++ src/tests/server.test.ts
@@ -1,83 +1,26 @@
-import { superdeno } from "https://deno.land/x/superdeno@4.2.1/mod.ts";
-import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.12-alpha/deno-dom-wasm.ts";
 // @deno-types="https://unpkg.com/@types/mocha@7.0.2/index.d.ts"
 import "https://unpkg.com/mocha@7.2.0/mocha.js";
 import { expect } from "https://deno.land/x/expect@v0.2.9/mod.ts";
-import app from "../server.ts";
+import { suit, describe } from './test_helper.ts';
 
-mocha.setup({ ui: "bdd", reporter: "spec" });
+suit(() => {
+  describe("/", x => {
+    it("should return 200 OK", () => x.expect(200));
+    it("should return HTML", () => x.expect("Content-Type", "text/html; charset=utf-8"));
+    it("should contain EUR currency", () => x.expect(".currencies .eur a").toEqual(e => e.textContent, "EUR"));
+  });
 
-function onCompleted(failures: number): void {
-  if (failures > 0) {
-      Deno.exit(1);
-  } else {
-      Deno.exit(0);
-  }
-}
+  describe("/eur", x => {
+    it("should return 200 OK", () =>  x.expect(200));
+    it("should return HTML", () =>  x.expect("Content-Type", "text/html; charset=utf-8"));
+    it("EUR should be chosen", () => x.expect(".currencies .eur.chosen a").toEqual(e => e.textContent, "EUR"));
+    it("should include Austria", () => x.expect(".countries .at .name").toEqual(e => e.textContent, "Austria"));
+  });
 
-describe("/", () => {
-  it("should return 200 OK", async () => await superdeno(app.handle.bind(app)).get("/").expect(200));
-  it("should return HTML", async () => await superdeno(app.handle.bind(app)).get("/").expect("Content-Type", "text/html; charset=utf-8"));
-  it("should contain EUR currency", async () => {
-    await superdeno(app.handle.bind(app))
-      .get("/")
-      .expect(response => {
-        const doc = new DOMParser().parseFromString(response.text, "text/html")!;
-        const currency = doc.querySelector(".currencies .eur a")!;
-        return expect(currency.textContent).toEqual("EUR");
-      });
+  describe("/eur?offset=20&limit=5", x => {
+    it("should return 200 OK", () =>  x.expect(200));
+    it("should return HTML", () =>  x.expect("Content-Type", "text/html; charset=utf-8"));
+    it("EUR should be chosen", () =>  x.expect(".currencies .eur.chosen a").toEqual(e => e.textContent, "EUR"));
+    it("should include Mayotte", () =>  x.expect(".countries .yt .name").toEqual(e => e.textContent, "Mayotte"));
   });
 });
-
-describe("/eur", () => {
-  const url = "/eur";
-
-  it("should return 200 OK", async () => await superdeno(app.handle.bind(app)).get(url).expect(200));
-  it("should return HTML", async () => await superdeno(app.handle.bind(app)).get(url).expect("Content-Type", "text/html; charset=utf-8"));
-  it("EUR should be chosen", async () => {
-    await superdeno(app.handle.bind(app))
-      .get(url)
-      .expect(response => {
-        const doc = new DOMParser().parseFromString(response.text, "text/html")!;
-        const currency = doc.querySelector(".currencies .eur.chosen a")!;
-        return expect(currency.textContent).toEqual("EUR");
-      });
-  });
-  it("should include Austria", async () => {
-    await superdeno(app.handle.bind(app))
-      .get(url)
-      .expect(response => {
-        const doc = new DOMParser().parseFromString(response.text, "text/html")!;
-        const country = doc.querySelector(".countries .at .name")!;
-        return expect(country.textContent).toEqual("Austria");
-      });
-  });
-});
-
-describe("/eur?offset=20&limit=5", () => {
-  const url = "/eur?offset=20&limit=5";
-
-  it("should return 200 OK", async () => await superdeno(app.handle.bind(app)).get(url).expect(200));
-  it("should return HTML", async () => await superdeno(app.handle.bind(app)).get(url).expect("Content-Type", "text/html; charset=utf-8"));
-
-  it("EUR should be chosen", async () => {
-    await superdeno(app.handle.bind(app))
-      .get(url)
-      .expect(response => {
-        const doc = new DOMParser().parseFromString(response.text, "text/html")!;
-        const currency = doc.querySelector(".currencies .eur.chosen a")!;
-        return expect(currency.textContent).toEqual("EUR");
-      });
-  });
-  it("should include Mayotte", async () => {
-    await superdeno(app.handle.bind(app))
-      .get(url)
-      .expect(response => {
-        const doc = new DOMParser().parseFromString(response.text, "text/html")!;
-        const country = doc.querySelector(".countries .yt .name")!;
-        return expect(country.textContent).toEqual("Mayotte");
-      });
-  });
-});
-
-mocha.run(onCompleted).globals(["onerror"])
---
+++ src/tests/test_helper.ts
@@ -0,0 +1,77 @@
+import { superdeno, IResponse, Test } from "https://deno.land/x/superdeno@4.2.1/mod.ts";
+import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.12-alpha/deno-dom-wasm.ts";
+import type { Application } from "https://deno.land/x/oak@v6.0.1/mod.ts";
+import type { Element } from "https://deno.land/x/deno_dom@v0.1.12-alpha/deno-dom-wasm.ts";
+import { expect } from "https://deno.land/x/expect@v0.2.9/mod.ts";
+// @deno-types="https://unpkg.com/@types/mocha@7.0.2/index.d.ts"
+import "https://unpkg.com/mocha@7.2.0/mocha.js";
+import app from "../server.ts";
+
+interface Assertion {
+  (element: Element): void;
+}
+
+interface Void {
+  (): void;
+}
+
+interface ElementSelector {
+  (element: Element): any;
+}
+
+class HtmlExpectation {
+  public constructor(private readonly test: Test, private readonly query: string) {}
+  public toEqual(elementSelector: ElementSelector, comparison: any): Test {
+    return this.test.expect(response => {
+      const doc = new DOMParser().parseFromString(response.text, "text/html")!;
+      const element = doc.querySelector(this.query)!;
+      const e = elementSelector(element);
+      return expect(e).toEqual(comparison);
+    });
+  }
+}
+
+class TestContext {
+  public constructor(public readonly url: string,
+                     public readonly app: Application) {}
+
+  public expect(status: number): Test;
+  public expect(query: string): HtmlExpectation;
+  public expect(field: string, value: string): Test;
+  public expect(...args: any[]): Test | HtmlExpectation {
+    const test = superdeno(app.handle.bind(app)).get(this.url);
+
+    if (args.length == 1) {
+      if (typeof(args[0]) === 'number') {
+        return test.expect(args[0]);
+      }
+
+      return new HtmlExpectation(test, args[0]);
+    }
+
+    return test.expect(args[0], args[1]);
+  }
+}
+
+interface Describe {
+  (state: TestContext): void;
+}
+
+export function describe(url: string, fn: Describe) {
+  const context = new TestContext(url, app);
+  Mocha.describe(url, () => fn(context));
+}
+
+export function suit(fn: Void) {
+  mocha.setup({ ui: "bdd", reporter: "spec" });
+  fn();
+  mocha.run(onCompleted).globals(["onerror"])
+}
+
+function onCompleted(failures: number): void {
+  if (failures > 0) {
+      Deno.exit(1);
+  } else {
+      Deno.exit(0);
+  }
+}
